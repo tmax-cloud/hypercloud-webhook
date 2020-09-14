f3f2ca452e556bb984eabe5bf21849332e97aae5
sangwon_cho <sangwon_cho@tmax.co.kr>
2020-09-14 13:22:49 +0900

    [mod] init queue

--- src/main/java/k8s/example/client/Main.java
+++ src/main/java/k8s/example/client/Main.java
@@ -4,6 +4,8 @@ package k8s.example.client;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import k8s.example.client.audit.AuditController;
+
 public class Main {
 	public static Logger logger = LoggerFactory.getLogger("Webhook");
 	public static void main(String[] args) {
@@ -11,6 +13,10 @@ public class Main {
 			// Start webhook server
 			logger.info("[Main] Start webhook server");
 			new WebHookServer();
+			
+			// Start Audit
+			logger.info("[Main] Start Audit controller");
+			AuditController.start();
 			
 		} catch (Exception e) {
 			e.printStackTrace();
